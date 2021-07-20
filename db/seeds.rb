380e9a90671aa4d8e375d33c1f0e6cb09696f9de
ZaidYounan <zaid.younan@gmail.com>
2021-07-21 02:10:03 +1000

    Edit seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 Gym.create(name: "Zaid's Gym", address: "123, Fake Street")
 
-Boxer.create(id: 1, first_name: "John", last_name: "Smith", height: 176, weight: "74", reach: nil, stance: "orthodox", gym_id: 1)
+Gym.create(name: "Mitch's Gym", address: "456, Fake Street")
 
-Boxer.create(id: 2, first_name: "Eric", last_name: "Corban", height: 182, weight: "84", reach: nil, stance: "orthodox", gym_id: 1)
+Boxer.create(id: 1, first_name: "John", last_name: "Smith", height: 176, weight: "74", reach: nil, stance: "orthodox", gym_id: 7)
+
+Boxer.create(id: 2, first_name: "Eric", last_name: "Corban", height: 182, weight: "84", reach: nil, stance: "orthodox", gym_id: 8)
