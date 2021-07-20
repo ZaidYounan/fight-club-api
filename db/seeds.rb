0d838fe19e855a139ba78c405b71a80ffa649561
ZaidYounan <zaid.younan@gmail.com>
2021-07-20 19:51:40 +1000

    Add seeds for gym placeholder, and two boxers, to begin testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Gym.create(name: "Zaid's Gym", address: "123, Fake Street")
+
+Boxer.create(id: 1, first_name: "John", last_name: "Smith", height: 176, weight: "74", reach: nil, stance: "orthodox", gym_id: 1)
+
+Boxer.create(id: 2, first_name: "Eric", last_name: "Corban", height: 182, weight: "84", reach: nil, stance: "orthodox", gym_id: 1)
