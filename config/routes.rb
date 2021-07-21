e247b303897a5571668a1f518f2db90a0f020ec2
ZaidYounan <zaid.younan@gmail.com>
2021-07-21 20:52:20 +1000

    Simplify route for new boxer creation

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :coaches
   resources :boxers
   resources :gyms
+
+  post '/boxers/new' => 'boxers#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
