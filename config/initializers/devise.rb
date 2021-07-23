9d721ed7b525872c6722f74283efdea685296bc2
ZaidYounan <zaid.younan@gmail.com>
2021-07-23 14:39:05 +1000

    Reduce JWT token timer to 15 minutes expose Authorization on cors.rb, edit REST requests file for future tests

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,6 +12,7 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
+    jwt.expiration_time = 15.minutes.to_i
   end
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
