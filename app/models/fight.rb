2b40d7c54c30b40c3dedbac9beb9a3efa48e23b3
Mitchell Guthmann <79577106+mitchguthmann@users.noreply.github.com>
2021-07-23 23:28:34 +1000

    adjusted boxer and fight model relations

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -1,8 +1,6 @@
 class Fight < ApplicationRecord
   belongs_to :boxer_a, class_name: "Boxer"
   belongs_to :boxer_b, class_name: "Boxer"
-  belongs_to :coach
   belongs_to :winner, class_name: "Boxer"
   belongs_to :loser, class_name: "Boxer"
-  belongs_to :gym
 end
