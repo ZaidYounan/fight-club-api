adafc9d5b81ca567750b9a67f7d82715735ccb89
ZaidYounan <zaid.younan@gmail.com>
2021-07-26 00:04:32 +1000

    Edit Cors with client deployment URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://fight-club-mz.netlify.app/', 'http:/localhost:4000'
 
     resource '*',
       headers: :any,
