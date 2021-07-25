4e432d70df63f5f1c1c30d574f49f56912501e32
ZaidYounan <zaid.younan@gmail.com>
2021-07-26 00:57:47 +1000

    Change cors to attempt bug fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://fight-club-mz.netlify.app/', 'http:/localhost:4000',
+    origins 'https://fight-club-mz.netlify.app/'
 
     resource '*',
       headers: :any,
