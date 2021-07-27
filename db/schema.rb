458dbf86a1aa5cc880bf7de2abfa3710eb718c25
Mitchell Guthmann <79577106+mitchguthmann@users.noreply.github.com>
2021-07-27 22:00:10 +1000

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_052800) do
+ActiveRecord::Schema.define(version: 2021_07_27_113200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,8 +62,8 @@ ActiveRecord::Schema.define(version: 2021_07_25_052800) do
     t.datetime "time_scheduled"
     t.integer "rounds"
     t.integer "round_time"
-    t.bigint "winner_id", default: 0
-    t.bigint "loser_id", default: 0
+    t.bigint "winner_id"
+    t.bigint "loser_id"
     t.string "result"
     t.bigint "gym_id", null: false
     t.datetime "created_at", precision: 6, null: false
