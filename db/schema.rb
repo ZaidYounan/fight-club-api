7dd9756ec69cebca4d68dd5734fc913771db65d8
ZaidYounan <zaid.younan@gmail.com>
2021-07-27 22:24:23 +1000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2021_07_27_113200) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
