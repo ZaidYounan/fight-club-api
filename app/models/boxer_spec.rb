0c26e34c258203668465212689262391d43ae161
Mitchell Guthmann <79577106+mitchguthmann@users.noreply.github.com>
2021-07-27 11:26:23 +1000

    added tests for boxer

--- app/models/boxer_spec.rb
+++ app/models/boxer_spec.rb
@@ -1,10 +1,17 @@
-RSpec.describe Category, type: :model do
+RSpec.describe Boxer, type: :model do
     # Association test
-    it { should have_many(:books) }
+    it { should have_one_attached(:avatar) }
+    it { should belong_to(:gym) }
     # Validation tests
-    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:gym_id) }
     it {
       should validate_length_of(:name)
-        .is_at_least(3)
+        .is_at_least(1)
+    }
+    it {
+      should validate_length_of(:last_name)
+        .is_at_least(1)
     }
    end
